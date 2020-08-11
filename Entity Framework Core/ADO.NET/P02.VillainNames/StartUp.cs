47a8649493f4b07002003c195c0a47d970f869b2
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-08-11 17:18:55 +0300

    Add files via upload

---
+++ Entity Framework Core/ADO.NET/P02.VillainNames/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace P02.VillainNames
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string minionsConnectionString = @"Server=(localdb)\MSSQLLocalDB;Database=MinionsDB;Integrated security=true";
+            SqlConnection minionsSqlConnection = new SqlConnection(minionsConnectionString);
+            using (minionsSqlConnection)
+            {
+                minionsSqlConnection.Open();
+                string queryText = @"SELECT v.Name, COUNT(mv.VillainId) AS MinionsCount  
+                                        FROM Villains AS v 
+                                        JOIN MinionsVillains AS mv ON v.Id = mv.VillainId 
+                                        GROUP BY v.Id, v.Name 
+                                        HAVING COUNT(mv.VillainId) > 3 
+                                        ORDER BY COUNT(mv.VillainId)";
+                SqlCommand queryCommand = new SqlCommand(queryText, minionsSqlConnection);
+                SqlDataReader reader = queryCommand.ExecuteReader();
+                using (reader)
+                {
+                    while (reader.Read())
+                    {
+                        string name = reader["Name"]?.ToString();
+                        string numberOfMinions = reader.GetInt32(1).ToString();
+                        Console.WriteLine($"{name} - {numberOfMinions}");
+                    }
+                }
+
+            }
+
+        }
+    }
+}
